7398932fe33880d560d6659269ce0649b2be8d78
AYC93 <ayc.leo@gmail.com>
2023-04-14 15:11:56 +0800

    added sql

---
+++ rsvpdb.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM rsvpdb.rsvp
+LIMIT 0, 1000
+
+-- Date: 2023-04-14 15:11
+*/
+INSERT INTO `` (`ID`,`name`,`email`,`phone`,`confirmation_date`,`comments`) VALUES (1,'Tom M','Tom.M@example.com','555-1234','2023-03-17 10:00:00','Looking forward to the event!');
+INSERT INTO `` (`ID`,`name`,`email`,`phone`,`confirmation_date`,`comments`) VALUES (2,'Nick Smith','nick.smith@example.com','555-5678','2023-03-17 10:00:00','Can\'t wait!');
+INSERT INTO `` (`ID`,`name`,`email`,`phone`,`confirmation_date`,`comments`) VALUES (3,'Bob Johnson','bob.johnson@example.com','555-9012','2023-03-17 10:00:00','Not Sure');
+INSERT INTO `` (`ID`,`name`,`email`,`phone`,`confirmation_date`,`comments`) VALUES (4,'Alice Lee','alice.lee@example.com','555-3456','2023-03-17 10:00:00','Sorry, I won\'t be able to make it.');
+INSERT INTO `` (`ID`,`name`,`email`,`phone`,`confirmation_date`,`comments`) VALUES (5,'Samuel Jackson','samuel.jackson@example.com','433-543','2023-03-17 00:00:00','Excited to attend!');
+INSERT INTO `` (`ID`,`name`,`email`,`phone`,`confirmation_date`,`comments`) VALUES (11,'Peter Lynch','peter.lynch@example.com','421-243','2023-03-17 00:00:00','OK');
+INSERT INTO `` (`ID`,`name`,`email`,`phone`,`confirmation_date`,`comments`) VALUES (15,'Patrick Lynch','pat.lynch@example.com','421-243','2023-03-17 00:00:00','OK');
+INSERT INTO `` (`ID`,`name`,`email`,`phone`,`confirmation_date`,`comments`) VALUES (16,'Pete Lynch','pete.lynch@example.com','421-243','2023-03-17 00:00:00','OK');
